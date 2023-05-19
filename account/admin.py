b8def14d6ddaad07c9806055dc92a6f2c719d0d7
Frankie Vexx <franblog7@gmail.com>
2023-05-19 15:16:48 +0300

    updates

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import User
+from .models import  Certificate, Employee, Employer, User
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Employee)
+admin.site.register(Employer)
+admin.site.register(Certificate)
