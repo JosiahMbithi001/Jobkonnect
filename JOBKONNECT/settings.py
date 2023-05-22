3f0c404e44c4e72823d060a22d77f7953f793d19
joseph thuku <jthuku490@gmail.com>
2023-05-22 21:03:09 +0300

    added static, moved templates to parents dir created tes.html

--- JOBKONNECT/settings.py
+++ JOBKONNECT/settings.py
@@ -67,6 +67,8 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
+        #tell django where to find our templates
+        'DIRS': [str(BASE_DIR.joinpath('templates'))]
     },
 ]
 
@@ -118,7 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+#Tell Django Where to locate our static files
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))] 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
